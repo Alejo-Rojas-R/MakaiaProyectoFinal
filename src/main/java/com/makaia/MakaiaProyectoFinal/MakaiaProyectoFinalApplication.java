f74d60c73e9fd7e4dc983ff711430f6e9d57a6fc
Alejo-Rojas-R <alejandrorojas1152@gmail.com>
2023-12-22 20:18:09 -0500

    initial commit

---
+++ src/main/java/com/makaia/MakaiaProyectoFinal/MakaiaProyectoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.makaia.MakaiaProyectoFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakaiaProyectoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakaiaProyectoFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makaia/MakaiaProyectoFinal/MakaiaProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makaia.MakaiaProyectoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakaiaProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
