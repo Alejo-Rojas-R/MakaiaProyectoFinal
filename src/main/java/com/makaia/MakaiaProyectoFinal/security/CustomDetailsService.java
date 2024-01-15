c781ba0fe74795d24aa15ba0603b866adc8b6d15
JenniferMaigual99 <maigualdaniela@gmail.com>
2024-01-15 14:49:09 -0500

    se agregro clase custom service

---
+++ src/main/java/com/makaia/MakaiaProyectoFinal/security/CustomDetailsService.java
@@ -0,0 +1,39 @@
+package com.makaia.MakaiaProyectoFinal.security;
+
+import com.makaia.MakaiaProyectoFinal.entities.Usuario;
+import com.makaia.MakaiaProyectoFinal.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomDetailsService implements UserDetailsService {
+    private final UsuarioRepository usuarioRepository;
+
+    @Autowired
+    public CustomDetailsService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Usuario> user = this.usuarioRepository.findByEmail(username);
+
+        if(user.isEmpty()) {
+            throw new UsernameNotFoundException("Usuario no registrado");
+        }
+
+        UserDetails loggedUser = User
+                .withUsername(user.get().getEmail())
+                .password(user.get().getPassword())
+                .build();
+
+        return loggedUser;
+    }
+}
