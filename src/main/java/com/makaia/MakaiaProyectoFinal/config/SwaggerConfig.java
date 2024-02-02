1727bf6c27ab86c487295c37128a0f4b1e01c792
JenniferMaigual99 <maigualdaniela@gmail.com>
2024-02-02 16:14:31 -0500

    Documentacion Swagger-API desplegada

--- src/main/java/com/makaia/MakaiaProyectoFinal/config/SwaggerConfig.java
+++ src/main/java/com/makaia/MakaiaProyectoFinal/config/SwaggerConfig.java
@@ -10,21 +10,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-/*
- -> Acá definen el tipo de seguridad que van a utiilizar. Ésta configuración está para JWT, pero deben consultar como hacerlo para autenticación básica
-@SecurityScheme(
-        type = SecuritySchemeType.HTTP,
-        name = "BearerAuth",
-        scheme = "bearer",
-        bearerFormat = "JWT"
-)*/
+
 @OpenAPIDefinition(
         info=@Info(
                 title = "Evaluación y perfilamiento API"
                 ),
         servers = {
 
-             @Server(url ="https://makaiaproyectofinal-production.up.railway.app", description = "Servidor de producción"),
+             @Server(url ="https://makaiaproyectofinal-production.up.railway.app/", description = "Servidor de producción"),
                 @Server(url ="http://localhost:8082/", description = "Servidor local")
         }
 )
